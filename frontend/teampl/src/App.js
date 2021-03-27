a114f6ea276114fded75ff48d8d067447df6f717
ShinSeungMin <sky2197kr@gmail.com>
2021-03-28 03:18:34 +0900

    [UPDATE] 라우터 등록

--- frontend/teampl/src/App.js
+++ frontend/teampl/src/App.js
@@ -9,7 +9,18 @@ class App extends Component {
       <div>
         <div>헤더</div>
         <Route exact path='/' component={Home}/>
-        <Route path='/sign-up' component={SignUp}/>
+        <Route path='/sign-up' component={SignUp}/> {/*4*/}
+        <Route path='/log-in' component={SignUp}/> {/*3*/}
+        <Route path='/main' component={SignUp}/> {/*2, 5*/}
+
+        <Route path='/team/make' component={SignUp}/> {/*6*/} 
+        <Route path='/team/modify' component={SignUp}/> {/*6*/}
+        <Route path='/team/search' component={SignUp}/> {/*7, 12*/}
+        <Route path='/team/post' component={SignUp}/> {/*8*/}
+        <Route path='/team' component={SignUp}/> {/*9*/}
+
+        <Route path='/team/detail' component={SignUp}/> {/*10*/}
+        <Route path='/team/detail/post' component={SignUp}/> {/*11*/}
         <div>푸터</div>
       </div>
     )

93defa71abc6093346cbe91a3097021634663ce1
ShinSeungMin <sky2197kr@gmail.com>
2021-03-31 01:18:58 +0900

    [UPDATE] 공통 css 추가 및 글씨체 추가

--- frontend/teampl/src/App.js
+++ frontend/teampl/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom'
-import './App.css';
 import { Home, SignUp } from './pages'
 import Header from './components/Header/Header'
+import './App.scss';
 
 class App extends Component {
   render() {

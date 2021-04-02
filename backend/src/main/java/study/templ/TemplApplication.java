d6f9edbe83bf372fadcb1e2e5a5781e20055e18f
Subin Min <tnqls5417@naver.com>
2021-04-02 16:05:57 +0900

    [Backend] 초기 프로젝트 세팅

---
+++ backend/src/main/java/study/templ/TemplApplication.java
@@ -0,0 +1,13 @@
+package study.templ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/study/templ/TemplApplicationTests.java
@@ -0,0 +1,13 @@
+package study.templ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

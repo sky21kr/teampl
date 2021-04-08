16d38c32d886df6d23902fab99e2259b4cd89efb
Subin Min <tnqls5417@naver.com>
2021-04-08 21:50:19 +0900

    [Backend] team과 연관된 user 부분 구현

--- backend/src/main/java/study/templ/domain/User.java
+++ backend/src/main/java/study/templ/domain/User.java
@@ -1,12 +1,31 @@
 package study.templ.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Table(name = "user_account")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    Long userid;
+    Integer userid;
+    @NonNull
+    @Column(name = "account_id")
+    String accountid;
+    @NonNull
+    String password;
+    @NonNull
+    String nickname;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "owner",cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
+    List<Team> ownteams;
+
 }
--- backend/src/main/java/study/templ/repository/UserRepository.java
+++ backend/src/main/java/study/templ/repository/UserRepository.java
@@ -3,5 +3,5 @@ package study.templ.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.templ.domain.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 }

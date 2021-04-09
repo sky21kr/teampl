e5195002caa75f15e927c59442e35fec7b4a69bd
ShinSeungMin <sky2197kr@gmail.com>
2021-04-10 02:28:37 +0900

    [Frontend] LogIn 페이지 마크업 완료

--- frontend/teampl/src/components/LogIn/LogIn.js
+++ frontend/teampl/src/components/LogIn/LogIn.js
@@ -2,15 +2,26 @@ import React from 'react';
 import './LogIn.scss'
 
 const LogIn = () => {
+
+    const inputStyle = { width: '350px'}
+    const buttonStyle = { marginTop: '40px', marginBottom: '40px'}
+
     return (
         <div className="logIn">
             <div className="logInTitle">
                 팀플을 만들고 찾아보세요!
             </div>
-            <input className="t-input" placeholder="아이디"/>
-            <input className="t-input" placeholder="비밀번호"/>
-            <button>로그인</button>
-            <div>아직 회원이 아니신가요? 회원가입하기</div>
+            <input className="t-input" style={inputStyle} placeholder="아이디"/>
+            <input className="t-input" style={inputStyle} placeholder="비밀번호"/>
+            <button className="t-button" style={buttonStyle}>
+                로그인
+            </button>
+            <div className="signUpPhrase">
+                아직 회원이 아니신가요?
+                <span>
+                    회원가입하기
+                </span>
+            </div>
         </div>
     )
 }

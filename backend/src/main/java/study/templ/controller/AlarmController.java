88bfdb3b1c74e9e08a87f4734509dfd38db3028d
heyazoo1007 <heyazoo1007@naver.com>
2021-05-29 20:12:06 +0900

    [Backend] 알림 컨트롤러 수정

--- backend/src/main/java/study/templ/controller/AlarmController.java
+++ backend/src/main/java/study/templ/controller/AlarmController.java
@@ -1,11 +1,13 @@
 package study.templ.controller;
 
+import lombok.Getter;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import study.templ.domain.*;
 import study.templ.repository.AlarmRepository;
 import study.templ.service.AlarmService;
@@ -19,7 +21,7 @@ import java.util.List;
 import java.util.Optional;
 
 //알림 정보를 db에 저장하는 방식으로 해야함 로그인시 사용자한테 보내는 용도
-@Controller
+@RestController
 public class AlarmController {
 
     @Autowired
@@ -36,18 +38,27 @@ public class AlarmController {
 
 
     @GetMapping("/alarm")
-    public List<Alarm> sendMessages(@RequestBody HashMap<String, Object> param){
+    public List<Alarm> sendMessages(@RequestBody requestAlarm userid){
 
-        int userid = (int)param.get("userid");
-        if (param.get("userid")==null)
-            throw new EntityNotFoundException();
 
-        return alarmService.updateAlarm(userid);
+        if(userid.getUserid()==null)
+            throw new EntityNotFoundException("userid is null");
+        int id = userid.getUserid();
+        userService.getUserById(id);
+
+        return alarmService.updateAlarm(id);
 
     }
-
+    @Getter
+    static class requestAlarm {
+        @NonNull
+        Integer userid;
+    }
 
 }
+
+
+
 
 
 

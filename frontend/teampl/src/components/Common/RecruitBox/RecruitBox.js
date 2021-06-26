bf0124560313bd631a2e34d6f2b084266b0898cc
신승민 <sky2197kr@gmail.com>
2021-06-27 02:10:31 +0900

    [Frontend] 팀 검색 데이터 적용

--- frontend/teampl/src/components/Common/RecruitBox/RecruitBox.js
+++ frontend/teampl/src/components/Common/RecruitBox/RecruitBox.js
@@ -1,34 +1,25 @@
 import React from 'react'
 import styled, { css } from 'styled-components';
 
-const RecruitBox = (props) => {
-
-    const type = props.type
-    const status = props.status
-    const title = props.title
-    const contents = props.contents
-    const writer = props.writer
-    const date = props.date
-    const comments = props.comments
-
+const RecruitBox = ({ teamData }) => {
     const Subject = styled.span`
       text-align: center;
       font-size: 13px;
       padding: 3px 13px 5px;
       background:lightgray;
-      ${props=>(props.type === "공부/학문" && css`
+      ${props=>(props.category === 0 && css`
       background:#4461D7;`)
-       || (props.type === "취업/취준" && css` 
+       || (props.category === 1 && css` 
       background: #59C894;`)
-      || (props.type === "운동/스포츠" && css`
+      || (props.category === 2 && css`
         background:#F45721;`)
-      ||(props.type === "취미/오락" && css `
+      ||(props.category === 3 && css `
       background:#F03063;`)
-      || (props.type === "어학/자격증" && css`
+      || (props.category === 4 && css`
       background:#FEB51F;`)
-      ||(props.type === "IT/개발" && css`
+      ||(props.category === 5 && css`
       background:#895EF7;`)
-      ||(props.type === "기타" && css`
+      ||(props.category === 6 && css`
       background:#1FCBDC;
       `)}
       color: #fff;
@@ -39,10 +30,10 @@ const RecruitBox = (props) => {
         padding: 4px 7px 2px;
         font-size: 11px;
         line-height: 12px;
-        ${props =>(props.status === "모집중" && css`
+        ${props =>(props.status === true && css`
         color: #ff3e3e;
         border: 1px solid #ff3e3e;`)
-        ||(props.status === "모집완료" && css`
+        ||(props.status === false && css`
         color: #c4c4c4;
         border: 1px solid #eeeeee;
         `)};
@@ -51,36 +42,35 @@ const RecruitBox = (props) => {
     `
 
 
+    // category: 2
+    // datetime: "2021-05-29T11:37:54"
+    // introduction: "7new"
+    // limit: 4
+    // numberofmembers: 4
+    // status: true
+    // teamid: 363
+    // title: "7new"
+
 
     return (
         <div className="recruitBox">
             <div>
-                <Subject {...props}>{type}</Subject>
-                <Status {...props}>{status}</Status>
+                <Subject {...teamData}>{teamData.category}</Subject>
+                <Status {...teamData}>{teamData.status === true ? '모집중' : '모집완료'}</Status>
             </div>
             <div>
-                <h3>{title}</h3>
-                <p>{contents}</p>
+                <h3>{teamData.title}</h3>
+                <p>{teamData.introduction}</p>
             </div>
             <div>
                 <div>
-                <span>{writer}</span>
-                <span>{date}</span>
+                <span>{teamData.writer}</span>
+                <span>{teamData.datetime}</span>
                 </div>
-                <span><img src="/images/message-circle.svg"></img>{comments}</span>
+                <span><img src="/images/message-circle.svg"></img>{teamData.comments}</span>
             </div>
         </div>
     )
-}
-
-RecruitBox.defaultProps={
-    type : "카테고리",
-    status : "모집정보",
-    title: "글 제목",
-    contents : "글 내용",
-    writer : "글쓴이",
-    date : "날짜",
-    comments : "댓글 수"
 }
 
 export default RecruitBox;

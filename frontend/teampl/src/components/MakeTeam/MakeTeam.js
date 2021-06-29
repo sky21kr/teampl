3f4c86581b5e2cfc2c4a86aea15e0c047fc8ed21
신승민 <sky2197kr@gmail.com>
2021-06-30 01:30:42 +0900

    [Frontend] make-team 수정중

--- frontend/teampl/src/components/MakeTeam/MakeTeam.js
+++ frontend/teampl/src/components/MakeTeam/MakeTeam.js
@@ -2,11 +2,27 @@ import React, { Component, useState } from 'react';
 import './MakeTeam.scss'
 import DefaultModal from '../Common/Modal/DefaultModal/DefaultModal';
 import ImgSrc from '@/assets/images/createsuccess.svg';
+import { customAxios } from '@/lib/customAxios';
 
 function MakeTeam(){
-
-
     const [ showModal, setShowModal ] = useState(false);
+
+    const clickMakeTeam = async (e) => {
+        e.preventDefault();
+
+        const teamData = {
+            limit: 0,
+            owner: 0,
+            status: true,
+            title: e.target.title.value,
+            category: e.target.category.value,
+            numberofmembers: e.target.numberofmembers.value,
+            introduction: e.target.introduction.value,
+        }
+
+        await customAxios.post('/make-team', teamData)
+        console.log(teamData)
+    }
 
     const openModal = () => {
         setShowModal(true);
@@ -15,38 +31,32 @@ function MakeTeam(){
     const closeModal = () => {
         setShowModal(false);
     }
-
-
         return(
             <>
             <div className="makeTeamWrap">
                 <h3>직접 팀을 만들어보세요!</h3>
-                <form onSubmit={(e)=>{
-                    e.preventDefault();
-                    console.log(e.target.teamName.value,e.target.teamSubject.value,e.target.numberOfMember.value,e.target.teamIntro.value)
-                }}>
-                    <input className="teamName" type="text" name="teamName" placeholder="팀 이름" required></input>
-                    <select name="teamSubject">
+                <form onSubmit={clickMakeTeam}>
+                    <input className="teamName" type="text" name="title" placeholder="팀 이름" required></input>
+                    <select name="category">
                         <option disabled selected hidden>주제 분류</option>
-                        <option>공부/학문</option>
-                        <option>운동/스포츠</option>
-                        <option>취미/오락</option>
-                        <option>취업/취준</option>
-                        <option>어학/자격증</option>
-                        <option>IT/개발</option>
-                        <option>기타</option>
+                        <option value="1">공부/학문</option>
+                        <option value="2">운동/스포츠</option>
+                        <option value="3">취미/오락</option>
+                        <option value="4">취업/취준</option>
+                        <option value="5">어학/자격증</option>
+                        <option value="6">IT/개발</option>
+                        <option value="7">기타</option>
                     </select>
-                    <select name="numberOfMember">
+                    <select name="numberofmembers">
                         <option disabled selected hidden>팀 인원</option>
-                        <option>1명</option>
-                        <option>2명</option>
-                        <option>3명</option>
-                        <option>4명</option>
+                        <option value="1">1명</option>
+                        <option value="2">2명</option>
+                        <option value="3">3명</option>
+                        <option value="4">4명</option>
                     </select>
-                    <textarea className="teamIntro" name="teamIntro" placeholder="팀 소개글을 적어주세요!" required></textarea>
+                    <textarea className="teamIntro" name="introduction" placeholder="팀 소개글을 적어주세요!" required></textarea>
                     {/* form안의 내용이 작성,선택이 되어있을때만 openModal이 작동하게 if문을 추가해야됨 */}
-                    <button className="basicBtn" onClick={openModal}>팀 만들기</button>
-
+                    <button className="basicBtn">팀 만들기</button>
                 </form>
 
             </div>

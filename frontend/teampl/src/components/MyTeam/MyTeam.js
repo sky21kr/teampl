94eb04eac1f0a8c6cde0153c5750f9e81f4654c7
신승민 <sky2197kr@gmail.com>
2021-07-14 03:00:39 +0900

    [Frontend] 나의 팀 구현 중

--- frontend/teampl/src/components/MyTeam/MyTeam.js
+++ frontend/teampl/src/components/MyTeam/MyTeam.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Myinfo from './Myinfo';
 import MadeTeam from './MadeTeam';
 import JoinedTeam from './JoinedTeam';
+import { customAxios } from '@/lib/customAxios'
 import './MyTeam.scss'
 
 function MyTeam(){
+    const userId = sessionStorage.getItem('userid')
+
+    const [ myTeamList, setMyTeamList ] = useState([])
+    const [ memberTeamList, setMemberTeamList ] = useState([])
+
+
+    useEffect(async () => {
+        const myTeamResponse = (await customAxios.get('/myteam', { params: { userid: userId } } )).data;
+        setMyTeamList(myTeamResponse)
+
+        const memberTeamResponse = (await customAxios.get('/memberteam', { params: { userid: userId } } )).data;
+        setMemberTeamList(memberTeamResponse)
+
+    })
+
     return(
         <>
         <Myinfo></Myinfo>

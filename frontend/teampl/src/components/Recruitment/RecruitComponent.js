fd54593c81b5e382b3a74e3398174f49284da680
신승민 <sky2197kr@gmail.com>
2021-07-16 03:11:09 +0900

    [Frontend] Recruitment 슬라이더 수정 중

--- frontend/teampl/src/components/Recruitment/RecruitComponent.js
+++ frontend/teampl/src/components/Recruitment/RecruitComponent.js
@@ -1,35 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './RecruitComponent.scss';
 import RecruitBox from '@/components/Common/RecruitBox/RecruitBox';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function RecruitComponent(){
+function RecruitComponent({ teamList }){
+    const renderTeamList = teamList.map((teamData) => <RecruitBox key={teamData.teamid} teamData={teamData}/>)
+
     return(
         <div className="recruitBoxWrap">
-            <Link to="main" style={{textDecoration:'none'}}>
-                <RecruitBox></RecruitBox>
-            </Link>
-
-            <Link to="main" style={{textDecoration:'none'}}>
-             <RecruitBox type={"취업/취준"} status={"모집완료"}></RecruitBox>
-            </Link>
-            
-            <Link to="main" style={{textDecoration:'none'}}>
-             <RecruitBox type={"운동/스포츠"} status={"모집중"}></RecruitBox>
-            </Link>
-
-            <Link to="main" style={{textDecoration:'none'}}>
-              <RecruitBox type={"공부/학문"} status={"모집중"}></RecruitBox>
-            </Link>
-
-            <Link to="main" style={{textDecoration:'none'}}>
-                <RecruitBox type={"어학/자격증"} status={"모집중"}></RecruitBox>
-            </Link>
-
-            <Link to="main" style={{textDecoration:'none'}}>
-               <RecruitBox type={"취미/오락"} status={"모집중"}></RecruitBox>   
-            </Link>
-
+            {renderTeamList}
         </div>
     );
 }
--- frontend/teampl/src/components/Recruitment/Recruitment.js
+++ frontend/teampl/src/components/Recruitment/Recruitment.js
@@ -1,26 +1,56 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, {Pagination} from 'swiper';
+import SwiperCore, {Pagination, Virtual} from 'swiper';
+import React, { useEffect, useState, useLayoutEffect } from 'react';
+import { customAxios } from '@/lib/customAxios';
 
 // Import Swiper styles
 import 'swiper/swiper.scss';
 import 'swiper/components/pagination/pagination.scss';
 import RecruitComponent from './RecruitComponent';
 
-SwiperCore.use([Pagination]);
+SwiperCore.use([Pagination, Virtual]);
 
 function Recruitment(){
+
+  const [ teamList, setTeamList ] = useState([])
+  const [ pageNumber, setPageNumber ] = useState(0)
+
+  useLayoutEffect(() => {
+    if (teamList.length !== 0) { // plants가 빈 배열이 아니면'
+      console.log('hi', teamList)
+      const swiper = document.querySelector('.swiper-container').swiper;
+      console.log(swiper)
+      swiper.update();
+      swiper.slideTo(0);
+      // update 후 스크롤이 맨 처음 content 위치로 이동
+    }
+  }, [teamList]);
+
+  useEffect(async () => {
+    const fetchTeamList = (await customAxios.get('/team', { params: { pageNumber: 0, pageSize: 18}})).data.content
+    const tempTeamList = []
+
+    for(let i = 0 ; i < fetchTeamList.length / 6 ; i++) {
+      tempTeamList.push(fetchTeamList.slice(i*6, (i+1)*6))
+    }
+
+    console.log('tempTeamList', tempTeamList)
+    setTeamList(tempTeamList)
+  },[pageNumber])
+
+  const renderTeamList = teamList.map((team, index) => <SwiperSlide key={index}><RecruitComponent teamList={team}/></SwiperSlide>)
+
   return (
       <div className="recruitWrap">
       <h3>팀원을 구합니다</h3>
     <Swiper
-      spaceBetween={50}
       slidesPerView={1}
       pagination={{ clickable: true }}
     >
-      {/* <SwiperSlide><RecruitComponent></RecruitComponent></SwiperSlide>
-      <SwiperSlide><RecruitComponent></RecruitComponent></SwiperSlide>
-      <SwiperSlide><RecruitComponent></RecruitComponent></SwiperSlide> */}
+      {renderTeamList}
+      {/* <SwiperSlide><RecruitComponent></RecruitComponent></SwiperSlide> */}
+      {/* <SwiperSlide><RecruitComponent></RecruitComponent></SwiperSlide> */}
     </Swiper>
     </div>
   );

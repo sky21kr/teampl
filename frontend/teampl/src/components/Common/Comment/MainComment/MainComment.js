df63909713b227e9b28727f95b204a22491cd348
신승민 <sky2197kr@gmail.com>
2021-07-20 18:27:21 +0900

    [Frontend] 댓글 삭제 구현

--- frontend/teampl/src/components/Common/Comment/MainComment/MainComment.js
+++ frontend/teampl/src/components/Common/Comment/MainComment/MainComment.js
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MainComment.scss'
+import { customAxios } from '@/lib/customAxios';
+import DefaultModal from '../../Modal/DefaultModal/DefaultModal';
+import ImgSrc from '@/assets/images/askdelete.svg';
 
-const MainComment  = ({ writer, date, content, toggleReplyInput }) => {
+
+const MainComment  = ({ commentId, writer, date, content, toggleReplyInput }) => {
+    const [ showModal, setShowModal ] = useState(false)
+
+    const handleModify = () => {
+
+    }
+
+    const clickCommentDelete = () => {
+        setShowModal(true)
+    }
+
+    const handleDelete = () => {
+        customAxios.delete('delete?comment_id=35', { data: {
+            comment_id: commentId,
+        }})
+    }
+
+    const closeModal = () => {
+        setShowModal(false)
+    }
 
     return (
         <div className="mainComment"> 
             <div className="commentHeader">
-                <div>
+                <div className="commentInfo">
                     <span>{writer}</span>
                     <span>{date}</span>
                 </div>
-                <span onClick={toggleReplyInput}>대댓글</span>
+                <div className="commentFeature">
+                    <span onClick={toggleReplyInput}>대댓글</span>
+                    <span onClick={handleModify}>수정</span>
+                    <span onClick={clickCommentDelete}>삭제</span>
+                </div>
             </div>
             <div className="commentContent">
                 {content}
             </div>
+            <DefaultModal
+                showModal={showModal}
+                closeModal={closeModal}
+                imgSrc={ImgSrc}
+                title="댓글을 삭제하시겠습니까?"
+                btnOkText="삭제"
+                btnOk={handleDelete}
+                btnCancelText="취소"
+                btnCancel={closeModal}
+            />
         </div>
     )
 }

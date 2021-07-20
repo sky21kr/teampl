2792eb5262b912aca0d849127de5bc461bb84b5b
신승민 <sky2197kr@gmail.com>
2021-07-20 16:15:38 +0900

    [Frontend] 검색 및 enter 이벤트 추가

--- frontend/teampl/src/components/SearchArea/SearchArea.js
+++ frontend/teampl/src/components/SearchArea/SearchArea.js
@@ -16,11 +16,21 @@ function SearchArea() {
         setKeyword(e.target.value)
     }
 
+    const handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+          clickSearch();
+        }
+      };
+
     return(
         <div className="searchWrap"> 
             <h2 className="searchTitle">팀플을 만들고 찾아보세요!</h2>
             <div className="searchBox">
-                <input className="search" onChange={changeKeyword} type="text" placeholder="원하는 검색어를 입력해보세요!"/>
+                <input
+                    className="search"
+                    onChange={changeKeyword}
+                    onKeyPress={handleKeyPress}
+                    placeholder="원하는 검색어를 입력해보세요!"/>
                 <button onClick={clickSearch} >
                     <img src="/images/searchicon.svg"/>
                 </button>
--- frontend/teampl/src/components/SubMenu/SubMenu.js
+++ frontend/teampl/src/components/SubMenu/SubMenu.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SubMenu.scss'
 import SubBar from './SubBar/SubBar'
+import { useHistory } from 'react-router-dom';
 import SearchIcon from '@/assets/images/searchicon.svg'
 
 const SubMenu = () => {
+    const history = useHistory()
+
+    const [ keyword, setKeyword ] = useState('')
+
+    const clickSearch = () => {
+        history.push(`/team/search?keyword=${keyword}`)
+    }
+
+    const changeKeyword = (e) => {
+        setKeyword(e.target.value)
+    }
+
+    const handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+          clickSearch();
+        }
+      };
 
     return (
         <div className="subMenu">
             <SubBar />
             <div className="searchBar">
-                <input/>
-                <img src={SearchIcon}></img>
+                <input onChange={changeKeyword} onKeyPress={handleKeyPress}/>
+                <img src={SearchIcon} onClick={clickSearch}></img>
             </div>
         </div>
     )

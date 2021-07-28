b26365061650510a63df99de5d685c721edc7d2a
신승민 <sky2197kr@gmail.com>
2021-07-29 02:53:37 +0900

    [Frontend] axios intercept를 통한 token update 처리

--- frontend/teampl/src/lib/customAxios.ts
+++ frontend/teampl/src/lib/customAxios.ts
@@ -1,11 +1,25 @@
 import axios, { AxiosInstance } from 'axios';
 import { SERVER_ADDRESS } from '@/config';
-import Cookies from 'js-cookie';
 
 export const customAxios: AxiosInstance = axios.create({
     baseURL: `${SERVER_ADDRESS}`,
     headers: {
-        token: window.sessionStorage.getItem('token'),
-        userid: window.sessionStorage.getItem('userid'),
+        token: sessionStorage.getItem('token'),
+        userid: sessionStorage.getItem('userid'),
     },
 })
+
+customAxios.interceptors.request.use(
+    function (config) {
+        const token = sessionStorage.getItem('token')
+        const userId = sessionStorage.getItem('userid')
+
+        if(config.headers.token !== token) config.headers.token = token
+        if(config.headers.userid !== userId) config.headers.userid = userId
+
+        return config
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+)
